9247a1a71e216798dfaa7ab4bc8b9b3c63437b2f
Roberts Pumpurs <roberts@pumpurlabs.com>
2024-12-02 21:52:33 +0200

    feat: challenge -1

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
-use axum::{routing::get, Router};
+use axum::{
+    body::Body,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
 
 async fn hello_world() -> &'static str {
-    "Hello, world!"
+    "Hello, bird!"
+}
+
+async fn seek() -> Response {
+    Response::builder()
+        .status(StatusCode::FOUND)
+        .header("Location", "https://www.youtube.com/watch?v=9Gc4QTqslN4")
+        .body(Body::empty())
+        .unwrap()
 }
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new()
+        .route("/", get(hello_world))
+        .route("/-1/seek", get(seek));
 
     Ok(router.into())
 }
