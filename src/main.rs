9d55bf991c15f21b19d00ce6c4fe152bd27acbc5
Roberts Pumpurs <roberts@pumpurlabs.com>
2024-12-03 21:08:02 +0200

    feat: day 2 task 2

--- src/main.rs
+++ src/main.rs
@@ -47,12 +47,38 @@ async fn dest(params: Query<DestQParams>) -> String {
     destination.to_string()
 }
 
+#[derive(serde::Deserialize)]
+struct KeyQParams {
+    from: Ipv4Addr,
+    to: Ipv4Addr,
+}
+async fn key(params: Query<KeyQParams>) -> String {
+    let octets = [
+        params.to.octets()[0]
+            .overflowing_sub(params.from.octets()[0])
+            .0,
+        params.to.octets()[1]
+            .overflowing_sub(params.from.octets()[1])
+            .0,
+        params.to.octets()[2]
+            .overflowing_sub(params.from.octets()[2])
+            .0,
+        params.to.octets()[3]
+            .overflowing_sub(params.from.octets()[3])
+            .0,
+    ];
+    let destination = Ipv4Addr::from(octets);
+
+    destination.to_string()
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
         .route("/", get(hello_world))
         .route("/-1/seek", get(seek))
-        .route("/2/dest", get(dest));
+        .route("/2/dest", get(dest))
+        .route("/2/key", get(key));
 
     Ok(router.into())
 }
