2069a00dcf6dd551c6b3a2019316e9b05f084bf1
Roberts Pumpurs <roberts@pumpurlabs.com>
2024-12-03 21:15:02 +0200

    feat: day 2 task 3

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
-use std::net::Ipv4Addr;
+use std::{
+    net::{Ipv4Addr, Ipv6Addr},
+    ops::BitXor,
+};
 
 use axum::{
     body::Body,
     extract::Query,
+    handler::HandlerWithoutStateExt,
     http::StatusCode,
     response::{IntoResponse, Response},
     routing::get,
     Router,
 };
+use itertools::Itertools;
 
 async fn hello_world() -> &'static str {
     "Hello, bird!"
@@ -72,13 +77,60 @@ async fn key(params: Query<KeyQParams>) -> String {
     destination.to_string()
 }
 
+#[derive(serde::Deserialize)]
+struct V6DestQParams {
+    from: Ipv6Addr,
+    key: Ipv6Addr,
+}
+
+async fn v6_dest(params: Query<V6DestQParams>) -> String {
+    let mut segments = [0; 16];
+    for (idx, (from, key)) in params
+        .from
+        .octets()
+        .into_iter()
+        .zip_eq(params.key.octets())
+        .enumerate()
+    {
+        let value = from.bitxor(key);
+        segments[idx] = value;
+    }
+    let destination = Ipv6Addr::from(segments);
+
+    destination.to_string()
+}
+
+#[derive(serde::Deserialize)]
+struct V6KeyQParams {
+    from: Ipv6Addr,
+    to: Ipv6Addr,
+}
+async fn v6_key(params: Query<V6KeyQParams>) -> String {
+    let mut segments = [0; 16];
+    for (idx, (to, from)) in params
+        .to
+        .octets()
+        .into_iter()
+        .zip_eq(params.from.octets())
+        .enumerate()
+    {
+        let value = to.bitxor(from);
+        segments[idx] = value;
+    }
+    let destination = Ipv6Addr::from(segments);
+
+    destination.to_string()
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
         .route("/", get(hello_world))
         .route("/-1/seek", get(seek))
         .route("/2/dest", get(dest))
-        .route("/2/key", get(key));
+        .route("/2/key", get(key))
+        .route("/2/v6/dest", get(v6_dest))
+        .route("/2/v6/key", get(v6_key));
 
     Ok(router.into())
 }
