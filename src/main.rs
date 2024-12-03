796e2558098f6a82a4d9810c120a36182dd89b72
Roberts Pumpurs <roberts@pumpurlabs.com>
2024-12-03 21:02:52 +0200

    feat: day 2 task 1

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use std::net::Ipv4Addr;
+
 use axum::{
     body::Body,
+    extract::Query,
     http::StatusCode,
     response::{IntoResponse, Response},
     routing::get,
@@ -18,11 +21,38 @@ async fn seek() -> Response {
         .unwrap()
 }
 
+#[derive(serde::Deserialize)]
+struct DestQParams {
+    from: Ipv4Addr,
+    key: Ipv4Addr,
+}
+
+async fn dest(params: Query<DestQParams>) -> String {
+    let octets = [
+        params.from.octets()[0]
+            .overflowing_add(params.key.octets()[0])
+            .0,
+        params.from.octets()[1]
+            .overflowing_add(params.key.octets()[1])
+            .0,
+        params.from.octets()[2]
+            .overflowing_add(params.key.octets()[2])
+            .0,
+        params.from.octets()[3]
+            .overflowing_add(params.key.octets()[3])
+            .0,
+    ];
+    let destination = Ipv4Addr::from(octets);
+
+    destination.to_string()
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
         .route("/", get(hello_world))
-        .route("/-1/seek", get(seek));
+        .route("/-1/seek", get(seek))
+        .route("/2/dest", get(dest));
 
     Ok(router.into())
 }
