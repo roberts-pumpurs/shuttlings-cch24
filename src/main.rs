986be144bc2136e54b678bee50d5bed99efe3b62
Roberts Pumpurs <roberts@pumpurlabs.com>
2024-12-06 12:22:19 +0200

    feat: day 5 task 1

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ use std::{
 };
 
 use axum::{
-    body::Body,
+    body::{Body, Bytes},
     extract::Query,
     handler::HandlerWithoutStateExt,
-    http::StatusCode,
+    http::{HeaderMap, StatusCode},
     response::{IntoResponse, Response},
-    routing::get,
+    routing::{get, post},
     Router,
 };
 use itertools::Itertools;
@@ -122,6 +122,80 @@ async fn v6_key(params: Query<V6KeyQParams>) -> String {
     destination.to_string()
 }
 
+#[derive(serde::Deserialize, Debug)]
+struct Metadata {
+    orders: Vec<Order>,
+}
+
+#[derive(serde::Deserialize, Debug)]
+struct Order {
+    item: Option<toml::Value>,
+    quantity: Option<toml::Value>,
+}
+
+#[axum::debug_handler]
+async fn manifest(headers: HeaderMap, body: Bytes) -> Response {
+    let invalid_response = || Response::builder().status(204).body(Body::empty()).unwrap();
+    let invalid_manifest = || Response::builder().status(400).body(Body::empty()).unwrap();
+
+    let Some(content_type) = headers.get("Content-Type") else {
+        dbg!("content type header not present");
+        return invalid_response();
+    };
+    if content_type != "application/toml" {
+        dbg!("invalid content type");
+        return invalid_response();
+    }
+
+    let manifest = cargo_manifest::Manifest::<Metadata>::from_slice_with_metadata(&body);
+    let Ok(manifest) = manifest else {
+        dbg!(&manifest);
+        dbg!("invalid manifest");
+        return invalid_manifest();
+    };
+
+    let Some(metadata) = manifest.package.and_then(|m| m.metadata) else {
+        dbg!("metadata manifest key not present");
+        return invalid_response();
+    };
+
+    let (counter, valid_orders) = metadata
+        .orders
+        .into_iter()
+        .filter_map(|order| {
+            let item = match order.item? {
+                toml::Value::String(s) => s,
+                _ => None?,
+            };
+            let quantity: u32 = match order.quantity? {
+                toml::Value::Integer(integer) => integer.try_into().ok()?,
+                _ => None?,
+            };
+            Some((item, quantity))
+        })
+        .fold((0, "".to_owned()), |(mut counter, mut acc), i| {
+            if counter > 0 {
+                acc.push_str("\n");
+            }
+            acc.push_str(i.0.as_str());
+            acc.push_str(": ");
+            acc.push_str(i.1.to_string().as_str());
+            counter += 1;
+            (counter, acc)
+        });
+
+    if counter == 0 {
+        dbg!("no valid orders");
+        return invalid_response();
+    };
+
+    dbg!(&valid_orders);
+    Response::builder()
+        .status(200)
+        .body(Body::new(valid_orders))
+        .unwrap()
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
@@ -130,7 +204,8 @@ async fn main() -> shuttle_axum::ShuttleAxum {
         .route("/2/dest", get(dest))
         .route("/2/key", get(key))
         .route("/2/v6/dest", get(v6_dest))
-        .route("/2/v6/key", get(v6_key));
+        .route("/2/v6/key", get(v6_key))
+        .route("/5/manifest", post(manifest));
 
     Ok(router.into())
 }
